297040a82ce19818cf75bdfddf42386514f967f4
mailtokamalnayan <mailtokamalnayan@gmail.com>
2018-07-26 23:37:54 +0800

    Init

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <div className="Hero">
+      <div className="floaters">
+        <div className="CalloutText">
+        <h1>Factfulness is the stress-reducing habit of only carrying opinions for which you have strong supporting facts.</h1>
+        </div>
+        <div className="FormBox">
+          <button className="FormButton">Measure your factfulness</button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+)
+
+export default IndexPage
