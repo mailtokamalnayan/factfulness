0dd3a3f8206fd97ee263283cd6380e7fd5b1d0bd
mailtokamalnayan <mailtokamalnayan@gmail.com>
2018-07-28 19:43:19 +0800

    Responsive adjustments

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,7 +54,7 @@ const IndexPage = () => (
       </div>
     </div>
 
-      <svg className="svgWaves" width="100%" height="172" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg className="svgWaves" width="100%" height="100" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path fill="#f2f2f2">
         <animate repeatCount="indefinite" fill="freeze" attributeName="d" dur="20s" values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
 
